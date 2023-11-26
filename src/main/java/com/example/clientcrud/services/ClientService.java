055809c01c8d839319f07e68700c96eb5dd515ba
Pedro-lms <pedrodaluzmachado@gmail.com>
2023-11-25 22:24:54 -0300

    feat:finishing project

--- src/main/java/com/example/clientcrud/services/ClientService.java
+++ src/main/java/com/example/clientcrud/services/ClientService.java
@@ -3,6 +3,11 @@ package com.example.clientcrud.services;
 import com.example.clientcrud.dto.ClientDTO;
 import com.example.clientcrud.entities.Client;
 import com.example.clientcrud.repositories.ClientRepository;
+import com.example.clientcrud.services.exceptions.DatabaseException;
+import com.example.clientcrud.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -27,5 +32,44 @@ public class ClientService {
     public Page<ClientDTO> findAllPaged(Pageable pageable) {
         Page<Client> list = repository.findAll(pageable);
         return list.map(x -> new ClientDTO(x));
+    }
+
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id) {
+        Optional<Client> obj = repository.findById(id);
+        Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+        return new ClientDTO(entity);
+    }
+
+    @Transactional
+    public ClientDTO insert(ClientDTO dto) {
+        Client entity = new Client();
+        entity.setName(dto.getName());
+        entity = repository.save(entity);
+        return new ClientDTO (entity);
+    }
+
+    @Transactional
+    public ClientDTO update(Long id, ClientDTO dto) {
+        try {
+            Client entity = repository.getReferenceById(id);
+            entity.setName(dto.getName());
+            entity = repository.save(entity);
+            return new ClientDTO (entity);
+        }catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Id not found" + id);
+        }
+    }
+
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        }
+        catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException("Id not found " + id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Integrity violation");
+        }
     }
 }
