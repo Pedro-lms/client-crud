b8c97e36c2aa3c530707e7349fbd1061d889b9ec
Pedro-lms <pedrodaluzmachado@gmail.com>
2023-11-25 21:30:46 -0300

    fix: service page assigment

--- src/main/java/com/example/clientcrud/repositories/ClientRepository.java
+++ src/main/java/com/example/clientcrud/repositories/ClientRepository.java
@@ -3,5 +3,5 @@ package com.example.clientcrud.repositories;
 import com.example.clientcrud.entities.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClientRepository extends JpaRepository<Long, Client> {
+public interface ClientRepository extends JpaRepository<Client, Long> {
 }
